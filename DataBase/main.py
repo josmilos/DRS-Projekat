a5b9e31b0f84313662956889a9b5ca0674a40566
Milos <josipovic.pr105.2019@uns.ac.rs>
2022-12-03 21:39:09 +0100

    User Database CRUD functions

---
+++ DataBase/main.py
@@ -0,0 +1,19 @@
+from users import *
+
+start()
+#with app.app_context():
+# Program testing
+# new_user = User(
+# email="gmail@mail.com",
+# password="pass",
+# name="dFaqaw",
+# surname="dgfgffg",
+# address="gfsdhghjujuf",
+# phone="fg45h",
+# balance=0,
+# verified=False
+# )
+#
+# add_new_user(new_user)
+#
+# print(get_user_by_email("mail@mail.com").surname)
