fcecc3433c5897f14fcdfc2d27ed47a51a343733
Alen Eminović <eminovic.pr51.2019@uns.ac.rs>
2022-12-16 19:13:36 +0100

    Bug fix3

--- CryptoExchangeUI/UI_Project/main.py
+++ CryptoExchangeUI/UI_Project/main.py
@@ -44,14 +44,164 @@ def home():
 def logout():
     if "user" in session:
         session.pop("user",None)
-    cryptos = ["BTC", "ETH", "LTC", "BNB", "DOGE"]
-    cryptocurrency_prices = crypto_price(cryptos)
-    return render_template("index.html", crypto=cryptocurrency_prices)
+    return render_template("index.html")
 
 @app.route('/profil')
 def profil():
     user = session["user"]
     return render_template("profil.html", user=user)
+
+
+@app.route('/profile/verification', methods=["GET", "POST"])
+def verification():
+    user = session["user"]
+    email = user["user"]["email"]
+    if request.method == "POST":
+        owner = str(request.form["cowner"])
+        card_number = str(request.form["cnum"])
+        card_date = str(request.form["cdate"])
+        card_cvv = str(request.form["ccvv"])
+
+
+        parameters = {
+            "email": email,
+            "cowner": owner,
+            "cnum": card_number,
+            "cdate": card_date,
+            "ccvv": card_cvv
+        }
+
+        response = requests.patch("http://127.0.0.1:5000/exchange-crypto", params=parameters)
+        response.raise_for_status()
+        data = response.json()
+        print(data)
+
+
+@app.route('/profile/deposit', methods=["GET", "POST"])
+def deposit():
+    user = session["user"]
+    email = user["user"]["email"]
+    if request.method == "POST":
+        owner = str(request.form["cowner"])
+        card_number = str(request.form["cnum"])
+        card_date = str(request.form["cdate"])
+        card_cvv = str(request.form["ccvv"])
+        amount = float(request.form["amount"])
+
+        parameters = {
+            "email": email,
+            "cowner": owner,
+            "cnum": card_number,
+            "cdate": card_date,
+            "ccvv": card_cvv,
+            "amount": amount
+        }
+        response = requests.post("http://127.0.0.1:5000/deposit", params=parameters)
+        response.raise_for_status()
+        data = response.json()
+        print(data)
+
+
+@app.route('/profile/transaction-history', methods=["GET"])
+def transaction_history():
+    user = session["user"]
+    email = user["user"]["email"]
+
+    parameters = {
+        "email": email
+    }
+    response = requests.get("http://127.0.0.1:5000/user-transactions", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+
+    print(data)
+
+
+@app.route('/profile/wallet', methods=["GET"])
+def wallet():
+    user = session["user"]
+    email = user["user"]["email"]
+
+    parameters = {
+        "email": email
+    }
+    response = requests.get("http://127.0.0.1:5000/user-cryptocurrencies", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+
+    print(data)
+
+
+@app.route('/buy-crypto', methods=["GET", "POST", "PATCH"])
+def buy_crypto():
+    user = session["user"]
+
+    if request.method == "POST":
+        currency = str(request.form["currency"])
+        price = float(request.form["price"])
+        amount = float(request.form["amount"])
+        email = user["user"]["email"]
+
+        parameters = {
+            "email": email,
+            "curr": currency,
+            "price": price,
+            "amount": amount
+        }
+        response = requests.patch("http://127.0.0.1:5000/buy-crypto", params=parameters)
+        response.raise_for_status()
+        data = response.json()
+
+        print(data)
+
+
+@app.route('/sell-crypto', methods=["GET", "POST", "PATCH"])
+def sell_crypto():
+    user = session["user"]
+
+    if request.method == "POST":
+        currency = str(request.form["currency"])
+        price = float(request.form["price"])
+        amount = float(request.form["amount"])
+        email = user["user"]["email"]
+
+        parameters = {
+            "email": email,
+            "curr": currency,
+            "price": price,
+            "amount": amount
+        }
+        response = requests.patch("http://127.0.0.1:5000/sell-crypto", params=parameters)
+        response.raise_for_status()
+        data = response.json()
+
+        print(data)
+
+
+@app.route('/exchange', methods=["GET", "POST", "PATCH"])
+def exchange():
+    user = session["user"]
+    email = user["user"]["email"]
+    if request.method == "POST":
+        selling_currency = request.form["scurr"]
+        selling_price = float(request.form["sprice"])
+        selling_amount = float(request.form["samount"])
+        buying_currency = request.form["bcurr"]
+        buying_price = float(request.form["bprice"])
+
+        parameters = {
+            "email": email,
+            "scurr": selling_currency,
+            "sprice": selling_price,
+            "samount": selling_amount,
+            "bcurr": buying_currency,
+            "bprice": buying_price
+        }
+        response = requests.patch("http://127.0.0.1:5000/exchange-crypto", params=parameters)
+        response.raise_for_status()
+        data = response.json()
+
+        print(data)
 
 
 @app.route('/edit', methods=["GET", "POST", "PATCH"])
