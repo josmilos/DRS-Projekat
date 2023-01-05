6d8f160a002273d6e66e2fef3e90e0e9e66a24f0
Milos <josipovic.pr105.2019@uns.ac.rs>
2023-01-05 18:57:45 +0100

    Added new property for jsonify()

--- CryptoExchangeEngine/service/__init__.py
+++ CryptoExchangeEngine/service/__init__.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JSON_SORT_KEYS'] = False
 db = SQLAlchemy(app)
 
 from CryptoExchangeEngine.service import routes
